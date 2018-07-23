936f7639393559eb466880e65adc40f468ec4131
mcwilliamson <mark@gnarff.com>
2018-07-23 10:50:39 +0100

    Corrected @ComponentScan path

--- MarkWilliamsonWebTest/src/main/java/com/markwilliamson/productpackagemanagement/config/AppConfig.java
+++ MarkWilliamsonWebTest/src/main/java/com/markwilliamson/productpackagemanagement/config/AppConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "org.markwilliamson.resttest")
+@ComponentScan(basePackages = "com.markwilliamson.productpackagemanagement")
 @EnableTransactionManagement
 @PropertySource("classpath:application.properties")
 public class AppConfig{
